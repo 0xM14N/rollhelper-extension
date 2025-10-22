713ca1abd30f6b8bf50206905aed535bd77ee57c
0xM41N <43382783+0xM14N@users.noreply.github.com>
2025-10-22 10:28:06 +0200

    Update API URLs to use trader subdomain

--- src/Utils/utils.js
+++ src/Utils/utils.js
@@ -113,13 +113,13 @@ function uuidv4() {
 
 const createApiUrl = tabUrl => {
 	if (tabUrl.includes('csgorolltr.com')) {
-		return 'https://api.csgorolltr.com/graphql';
+		return 'https://api-trader.csgorolltr.com/graphql';
 	}
 	if (tabUrl.includes('csgoroll.com')) {
-		return 'https://api.csgoroll.com/graphql';
+		return 'https://api-trader.csgoroll.com/graphql';
 	}
 	if (tabUrl.includes('csgoroll.gg')) {
-		return 'https://api.csgoroll.gg/graphql';
+		return 'https://api-trader.csgoroll.gg/graphql';
 	}
 };
 
